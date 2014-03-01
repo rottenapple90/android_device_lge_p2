d361c3d766219c9e82b83eab7ed5afa7d63f0688
rottenapple90 <kin4288@naver.com>
2014-03-01 21:11:42 +0900

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/p760/p760.mk)
+$(call inherit-product, device/lge/p2/p2.mk)
 
 PRODUCT_NAME := cm_p760
 
@@ -17,11 +17,11 @@ PRODUCT_RELEASE_NAME := OptimusL9
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
 -include vendor/cm/config/common_versions.mk
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=u2_open_eu BUILD_FINGERPRINT=lge/u2_open_eu/u2:4.1.2/JRO03L/P76020b.4DAAAC8F:user/release-keys PRIVATE_BUILD_DESC="u2_open_eu-user 4.1.2 JRO03L P76020b.4DAAAC8F release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=p2_open_eu BUILD_FINGERPRINT=lge/u2_open_eu/u2:4.4.2/JRO03L/P94020b.4DAAAC8F:user/release-keys PRIVATE_BUILD_DESC="p2_open_ko-user 4.4.2 JRO03L P94020b.4DAAAC8F release-keys"
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := p760
+PRODUCT_DEVICE := p2
 
 # Enable Torch
-PRODUCT_PACKAGES += Torch Camera2 LgeL9Parts
+PRODUCT_PACKAGES += Torch Camera2 LgePrada3.0Parts
 
