125921337c0b461b9aa1d19d1933112f2281ac81
rottenapple90 <kin4288@naver.com>
2014-03-01 21:10:10 +0900

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,7 +8,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := omap4
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_BOOTLOADER_BOARD_NAME := p760
+TARGET_BOOTLOADER_BOARD_NAME := p2
 TARGET_CPU_SMP := true
 TARGET_ARCH := arm
 TARGET_CPU_VARIANT := cortex-a9
@@ -28,7 +28,7 @@ BUILD_FM_RADIO := true
 FM_CHR_DEV_ST := true
 BUILD_TI_FM_APPS := true
 # Try to build the kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_p760_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_p2_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/omap4-common
 #TARGET_PREBUILT_KERNEL := device/lge/p760/kernel
 
@@ -36,11 +36,11 @@ TARGET_KERNEL_SOURCE := kernel/lge/omap4-common
 
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUEDROID_VENDOR_CONF := device/lge/p760/bluetooth/vnd_lge_p760.txt
+BOARD_BLUEDROID_VENDOR_CONF := device/lge/p2/bluetooth/vnd_lge_p2.txt
 
 BOARD_HAS_NO_MISC_PARTITION := true
 
-TARGET_RECOVERY_FSTAB = device/lge/p760/fstab.u2
+TARGET_RECOVERY_FSTAB = device/lge/p2/fstab.p2
 RECOVERY_FSTAB_VERSION = 2
 BOARD_UMS_LUNFILE := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
 
@@ -52,7 +52,7 @@ TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 
 TARGET_BOOTLOADER_BOARD_NAME := p760
 
-BOARD_EGL_CFG := device/lge/p760/egl.cfg
+BOARD_EGL_CFG := device/lge/p2/egl.cfg
 BOARD_EGL_WORKAROUND_BUG_10194508 := true
 
 TARGET_USES_GL_VENDOR_EXTENSIONS := false
@@ -89,24 +89,24 @@ USE_OPENGL_RENDERER := true
 #BOARD_TOUCH_RECOVERY := true
 #ENHANCED_DOMX := true
 
-TARGET_SPECIFIC_HEADER_PATH := device/lge/p760/include
+TARGET_SPECIFIC_HEADER_PATH := device/lge/p2/include
 
-BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/lge/p760/vibrator.c
+BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/lge/p2/vibrator.c
 
 COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB  -DNEEDS_VECTORIMPL_SYMBOLS -DHAVE_FM_RADIO -DFORCE_SCREENSHOT_CPU_PATH
 
 
 KERNEL_SGX_MODULES:
-	make -C device/lge/p760/sgx-module/eurasia_km/eurasiacon/build/linux2/omap4430_android/ O=$(KERNEL_OUT) KERNELDIR=$(ANDROID_BUILD_TOP)/$(KERNEL_SRC) ARCH="arm" $(ARM_CROSS_COMPILE) KERNEL_CROSS_COMPILE=$(ARM_CROSS_COMPILE) TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.0
+	make -C device/lge/p2/sgx-module/eurasia_km/eurasiacon/build/linux2/omap4430_android/ O=$(KERNEL_OUT) KERNELDIR=$(ANDROID_BUILD_TOP)/$(KERNEL_SRC) ARCH="arm" $(ARM_CROSS_COMPILE) KERNEL_CROSS_COMPILE=$(ARM_CROSS_COMPILE) TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.0
 	mkdir -p $(TARGET_OUT)/modules/
 	mv $(OUT)/target/*sgx540_120.ko $(TARGET_OUT)/modules/
 
 TARGET_KERNEL_MODULES := KERNEL_SGX_MODULES
 
 ## Radio fixes
-BOARD_RIL_CLASS := ../../../device/lge/p760/ril/
+BOARD_RIL_CLASS := ../../../device/lge/p2/ril/
 
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p760/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p2/bluetooth
 
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
 
@@ -114,7 +114,7 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storag
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
 BOARD_SEPOLICY_DIRS := \
-    device/lge/p760/selinux
+    device/lge/p2/selinux
 
 BOARD_SEPOLICY_UNION := \
     file_contexts \
@@ -122,7 +122,7 @@ BOARD_SEPOLICY_UNION := \
     device.te \
     domain.te
 
-BOARD_HARDWARE_CLASS := device/lge/p760/cmhw/
+BOARD_HARDWARE_CLASS := device/lge/p2/cmhw/
 
 
 
